dcb46aa42c017acee19cc36e0314b2aaa15e463a
peter kiwanda <kiwandapeter32@gmail.com>
2024-10-17 01:21:49 +0300

    RecipeManagement api project

--- RecipeManagement/RecipeManagement/wsgi.py
+++ RecipeManagement/RecipeManagement/wsgi.py
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add your project directory to the sys.path
+project_home = '/home/peterkiwanda/RecipeManagement_API_Project/BE_Capstone_Project/RecipeManagement'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+
 
 from django.core.wsgi import get_wsgi_application
 
